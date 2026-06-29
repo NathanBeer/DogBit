28462bfb62586b231c1ba8eb354a08427ecb10c2
NathanBeer <inathanbr@gmail.com>
2026-06-30 01:07:11 +0300

    Fixing repository structure for submission

--- Simulation/RobotSeeing_Sim.py
+++
@@ -1,7 +0,0 @@
-import pybullet as p
-
-def look_for_object(target_id, robot_id, threshold=1.0):
-    pos_r, _ = p.getBasePositionAndOrientation(robot_id)
-    pos_t, _ = p.getBasePositionAndOrientation(target_id)
-    dist = ((pos_r[0]-pos_t[0])**2 + (pos_r[1]-pos_t[1])**2)**0.5
-    return dist < threshold
